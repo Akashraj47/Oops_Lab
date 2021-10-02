61780156115d0e2ecdd5379b4048d5b6bc9a3f96
Akashraj47 <91429658+Akashraj47@users.noreply.github.com>
2021-10-03 00:45:25 +0530

    Update task3.cpp

--- practical/task3.cpp
+++ practical/task3.cpp
@@ -1,17 +1,45 @@
 #include<iostream>
-namespace first()
+
+namespace first
+
 {
+
   int add(int a,int b)
+
+  {
+
+  
+
    return(a+b);
+
+   }
+
 }
-namespace second()
+
+namespace second
+
 {
+
   float add(float a,float b)
+
+  {
+
     return(a+b);
+
 }
+
+}
+
+using namespace std;
+
 int main()
+
 {
+
   cout << add(1,2) << endl;
+
   cout << add(1.5f,1.4f) << endl;
+
   return 0;
+
 }
