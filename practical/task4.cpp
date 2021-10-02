20feaab271ffbc3cd038c431b1adfa1973206285
Akashraj47 <91429658+Akashraj47@users.noreply.github.com>
2021-10-03 00:50:56 +0530

    Update task4.cpp

--- practical/task4.cpp
+++ practical/task4.cpp
@@ -3,12 +3,16 @@ using namespace std;
 namespace first
 {
   int add(int a,int b)
-    return(a+b);
+  {
+   return(a+b);
+  }
 }
 namespace second
 {
   float add(float a,float b)
+  {
     return(a+b);
+  }
 }
 using namespace first;
 using namespace second;
