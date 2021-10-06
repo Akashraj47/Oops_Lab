488f0ed8cf42cf2884e40552285793a7cb6c057a
Akashraj47 <91429658+Akashraj47@users.noreply.github.com>
2021-10-06 10:24:40 +0530

    Create task1.cpp

---
+++ practical_6/task1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string a ="1234";
+	int b = 123;
+	cout << a+b;
+	return 0;
+}#include <iostream>
+using namespace std;
+int main()
+{
+	string a ="1234";
+	int b = 123;
+	cout << a+b;
+	return 0;
+}
